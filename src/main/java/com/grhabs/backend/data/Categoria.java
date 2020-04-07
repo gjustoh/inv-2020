5d82577cdfac42f8375746ea79686fda3b071e2d
gabriel justo <gjusto@unsa.edu.pe>
2020-04-07 13:02:57 -0500

    configurando objeto Categoria.java

--- src/main/java/com/grhabs/backend/data/Categoria.java
+++ src/main/java/com/grhabs/backend/data/Categoria.java
@@ -1,30 +1,40 @@
 package com.grhabs.backend.data;
 
 public class Categoria {
-    private int idcategoria;
-    private String categoria;
+    private int id;
+    private String name;
+    private String estado;
 
     public Categoria() {
     }
 
-    public Categoria(int idcategoria, String categoria) {
-        this.idcategoria = idcategoria;
-        this.categoria = categoria;
+    public Categoria(int id, String name, String estado) {
+        this.id = id;
+        this.name = name;
+        this.estado = estado;
     }
 
-    public int getIdcategoria() {
-        return idcategoria;
+    public int getId() {
+        return id;
     }
 
-    public void setIdcategoria(int idcategoria) {
-        this.idcategoria = idcategoria;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getCategoria() {
-        return categoria;
+    public String getName() {
+        return name;
     }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
